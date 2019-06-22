120a8cf6fa66285214054e66d7d6803371c9ebe6
Ekimerton <ekim0252@gmail.com>
2019-06-22 22:29:06 +0300

    switched to environment variables

--- Driver/config.py
+++ Driver/config.py
@@ -2,13 +2,13 @@ import os
 
 class Config:
     # App config
-    SECRET_KEY = '086939e1532b75dc6d97c55b796d50e6'
-    SQLALCHEMY_DATABASE_URI = "sqlite:///douh.db"
+    SECRET_KEY = os.environ['SECRET_KEY']
+    SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
 
     # Reset mail login
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = 'douh.reset@gmail.com'
-    MAIL_PASSWORD = 'dough35461157$'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ['MAIL_SERVER']
+    MAIL_PORT = os.environ['MAIL_PORT']
+    MAIL_USE_TLS = os.environ['MAIL_USE_TLS']
+    MAIL_USERNAME = os.environ['MAIL_USERNAME']
+    MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ['SQLALCHEMY_TRACK_MODIFICATIONS']
